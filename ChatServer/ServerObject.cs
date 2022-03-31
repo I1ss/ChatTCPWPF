a1c6fa76e9a82c410f2f811fd4286ffa3c08b1a7
I1ss <lebedewgeorg@yandex.ru>
2022-03-31 21:59:06 +0700

    Tests Moq

---
+++ ChatServer/ServerObject.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace ChatServer
+{
+    public class ServerObject
+    {
+        static TcpListener tcpListener; 
+        public List<ClientObject> clients = new List<ClientObject>(); 
+        internal List<String> clients_names = new List<String>();
+        internal void SetNewName(String name)
+        {
+            clients_names.Add(name);
+        }
+        public void AddConnection(ClientObject clientObject)
+        {
+            clients.Add(clientObject);
+            return;
+        }
+        public void RemoveConnection(string id, string userName)
+        {
+            ClientObject client = clients.FirstOrDefault(c => c.Id == id);
+            if (client != null)
+            {
+                try
+                {
+                    clients.Remove(client);
+                    clients_names.Remove(userName);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return;
+        }
+        protected internal void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, 8888);
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                    
+                    ClientObject clientObject = new ClientObject(tcpClient, this);
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+        protected internal void BroadcastMessage(string message, string id)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Stream.Write(data, 0, data.Length); 
+            }
+        }
+
+        protected internal void BroadcastMessageInd(string name, string message, string userName)
+        {
+            message = String.Format("{0} лично тебе: {1}", userName, message);
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if (clients[i].userName == name) 
+                {
+                    clients[i].Stream.Write(data, 0, data.Length); 
+                }
+            }
+        }
+        protected internal void Disconnect()
+        {
+            tcpListener.Stop(); 
+
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Close();
+            }
+            Environment.Exit(0);
+        }
+    }
+}
